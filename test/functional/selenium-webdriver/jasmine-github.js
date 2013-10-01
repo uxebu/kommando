1778fa3286a17aab77e6365f21127ea1e41a3ae2
Tobias von Klipstein <klipstein@uxebu.com>
2013-10-01 22:41:50 +0200

    Rewrite pure jasmine tests

--- test/functional/selenium-webdriver/jasmine-github.js
+++ test/functional/selenium-webdriver/jasmine-github.js
@@ -2,12 +2,12 @@ describe('selenium-webdriver / jasmine', function() {
   describe('github', function() {
     it('reads the "title"', function(done) {
       kommando.browser.get('https://www.github.com').then(function() {
-        kommando.browser.findElement(kommando.webdriver.By.className('heading')).then(function(heading) {
-          heading.getText().then(function(text) {
-            expect(text).toBe('Build software better, together.');
-            done();
-          });
-        });
+        return kommando.browser.findElement(kommando.webdriver.By.className('heading'));
+      }).then(function(heading) {
+        return heading.getText();
+      }).then(function(text) {
+        expect(text).toBe('Build software better, together.');
+        done();
       });
     });
   });
--- test/functional/selenium-webdriver/jasmine-google-search.js
+++ test/functional/selenium-webdriver/jasmine-google-search.js
@@ -2,14 +2,14 @@ describe('selenium-webdriver / jasmine', function() {
   describe('google-search', function() {
     it('searches for "webdriver"', function(done) {
       kommando.browser.get('http://www.google.de').then(function() {
-        kommando.browser.findElement(kommando.webdriver.By.name('q')).then(function(searchBox) {
-          searchBox.sendKeys('webdriver').then(function() {
-            searchBox.getAttribute('value').then(function(attribute) {
-              expect(attribute).toBe('webdriver');
-              done();
-            });
-          });
+        return kommando.browser.findElement(kommando.webdriver.By.name('q'));
+      }).then(function(searchBox) {
+        return searchBox.sendKeys('webdriver').then(function() {
+          return searchBox.getAttribute('value');
         });
+      }).then(function(attribute) {
+        expect(attribute).toBe('webdriver');
+        done();
       });
     });
   });
