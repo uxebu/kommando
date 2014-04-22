3cbf3c5ad83fb0c06e3c5a910717a502f676ce2a
Tobias von Klipstein <tk@uxebu.com>
2014-04-23 00:29:25 +0200

    Add standalone Chromedriver / PhantomJS driver

---
+++ src/driver/chromedriver.js
@@ -0,0 +1,47 @@
+var driverLauncher = require('../driver-launcher.js');
+
+var address = require('address');
+var freeport = require('freeport');
+var webdrvr = require('webdrvr');
+
+module.exports = function(options) {
+
+  var launcher = null;
+  var seleniumUrl = '';
+
+  return {
+    updateCapabilities: function(caps) {
+      return caps;
+    },
+    start: function(callback) {
+      console.log('Starting standalone Chromedriver ...');
+
+      freeport(function(err, port) {
+        var args = ['--port=' + port]
+          .concat(options.args || []);
+        
+        var driverLauncherOptions = {
+          args: args,
+          hostname: address.ip() || address.ip('lo'),
+          port: port,
+          path: ''
+        };
+        launcher = driverLauncher(webdrvr.chromedriver.path, driverLauncherOptions).start(function(error, url) {
+          if (error) {
+            callback(error);
+          } else {
+            console.log('Standalone Chromedriver started at: ' + url);
+            seleniumUrl = url;
+            callback(null, url);
+          }
+        }.bind(this));
+
+      }.bind(this));
+    },
+    stop: function(results, callback) {
+      console.log('Shutting down standalone Chromedriver at: ' + seleniumUrl);
+      launcher.stop(callback);
+    }
+  };
+
+};
---
+++ src/driver/phantomjs.js
@@ -0,0 +1,47 @@
+var driverLauncher = require('../driver-launcher.js');
+
+var address = require('address');
+var freeport = require('freeport');
+var webdrvr = require('webdrvr');
+
+module.exports = function(options) {
+
+  var launcher = null;
+  var seleniumUrl = '';
+
+  return {
+    updateCapabilities: function(caps) {
+      return caps;
+    },
+    start: function(callback) {
+      console.log('Starting standalone PhantomJS Ghostdriver ...');
+
+      freeport(function(err, port) {
+        var args = ['--webdriver=' + port]
+          .concat(options.args || []);
+        
+        var driverLauncherOptions = {
+          args: args,
+          hostname: address.ip() || address.ip('lo'),
+          port: port,
+          path: '/wd/hub'
+        };
+        launcher = driverLauncher(webdrvr.phantomjs.path, driverLauncherOptions).start(function(error, url) {
+          if (error) {
+            callback(error);
+          } else {
+            console.log('Standalone PhantomJS Ghostdriver started at: ' + url);
+            seleniumUrl = url;
+            callback(null, url);
+          }
+        }.bind(this));
+
+      }.bind(this));
+    },
+    stop: function(results, callback) {
+      console.log('Shutting down standalone PhantomJS Ghostdriver at: ' + seleniumUrl);
+      launcher.stop(callback);
+    }
+  };
+
+};
