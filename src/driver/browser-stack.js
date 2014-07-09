77d944f4e8f7108750e12c491813878d81d2f733
Tobias von Klipstein <tk@uxebu.com>
2014-07-10 01:35:07 +0200

    Extract duplication

--- src/driver/browser-stack.js
+++ src/driver/browser-stack.js
@@ -1,47 +1,8 @@
 'use strict';
 
-var url = require('url');
-
-var lodash = require('lodash');
-var Promise = require('digdug/node_modules/dojo/Promise');
+var digdugAdapter = require('../digdug.js');
 var DigdugBrowserStackTunnel = require('digdug/BrowserStackTunnel');
 
-var merge = lodash.merge;
-
 module.exports = function(options) {
-
-  var jobState = options.jobState;
-  delete options.jobState;
-
-  return {
-    _tunnel: null,
-    updateCapabilities: function(caps) {
-      return merge({}, this._tunnel.extraCapabilities, caps);
-    },
-    start: function(callback) {
-      var tunnel = this._tunnel = new DigdugBrowserStackTunnel(merge({
-        tunnelId: Date.now()
-      }, options));
-      tunnel.start().then(function(error) {
-        console.log('Using BrowserStack selenium server at: ' + tunnel.clientUrl);
-        callback(null, tunnel.clientUrl);
-      }, function(error) {
-        callback(error);
-      });
-    },
-    stop: function(results, callback) {
-      var sendJobStates = [];
-      var tunnel = this._tunnel;
-      results.forEach(function(result) {
-        (result.clientIds || []).forEach(function(clientId) {
-          sendJobStates.push(tunnel.sendJobState(clientId, merge({
-            success: result.passed
-          }, jobState)));
-      });
-      Promise.all(sendJobStates).then(function() {
-        return tunnel.stop();
-      }).then(callback, callback);
-    }
-  };
-
+  return digdugAdapter('BrowserStack', DigdugBrowserStackTunnel, options);
 };
